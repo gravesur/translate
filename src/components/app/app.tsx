92402d166281ebe41d5423c5877b0c2a45d0a426
gravesur <gravesur@example.com>
2021-02-16 00:20:30 +0700

    added basic structure

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,20 @@ import React from 'react';
 import './app.scss';
 
 const App = () => {
-  return <div>App</div>;
+  const sentence = 'Посетители в ресторане заказали пиццу';
+
+  return (
+    <div className="app">
+      <h1 className="app-header">Translate this sentence</h1>
+      <div className="app-sentence">
+        <p className="app-paragraph">{sentence}</p>
+      </div>
+      <div className="app-check">Check</div>
+      <div className="app-words"></div>
+      <h2 className="app-warning">Warning message!</h2>
+      <button className="app-check-button">Check</button>
+    </div>
+  );
 };
 
 export default App;
